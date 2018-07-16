1822196352ceb46442c01144c05d2a2635afd5ea
Michael OConnor <mikeoconnor20@gmail.com>
2018-07-16 14:43:48 -0400

    suggestion box for routing

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('SuggestionBox', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'HomeController',
+		templateUrl: 'views/home.html'
+	}).otherwise({
+		redirectTo: '/'
+	})
+});	
